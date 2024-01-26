21afea91cc1622a463f6fbb930b119ca6c32d8ae
« val » <bxl@projetfeudecamp.be>
2024-01-26 10:03:34 +0100

    test github action

---
+++ src/_data/articles_en.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+module.exports = async () => {
+  try {
+    const res = await axios.get('http://83.166.147.249:1337/api/articles?locale=en');
+    console.dir(res.data, { depth: null });
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching data from Strapi:', error);
+  }
+};
