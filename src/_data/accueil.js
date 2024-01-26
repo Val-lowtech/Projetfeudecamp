d26802ba317ae9af5d9a482dd30db61ee13bf56a
« val » <bxl@projetfeudecamp.be>
2024-01-26 06:39:38 +0100

    new api

--- src/_data/accueil.js
+++ src/_data/accueil.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async () => {
   try {
-    const res = await axios.get('https://backend-pfdc.jcloud.ik-server.com:1337/api/accueil?populate=*');
+    const res = await axios.get('http://195.15.237.156:1337/api/accueil?populate=*');
     console.dir(res.data, { depth: null });
     return res.data;
   } catch (error) {
