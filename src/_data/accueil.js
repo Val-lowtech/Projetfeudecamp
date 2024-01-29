c13fd13d5978fb6b0b0f5974244d5a13b9cb7644
« val » <bxl@projetfeudecamp.be>
2024-01-29 17:22:50 +0100

    quick fix

--- src/_data/accueil.js
+++ src/_data/accueil.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async () => {
   try {
-    const res = await axios.get('http://83.166.147.249:1337/api/accueil?populate=*');
+    const res = await axios.get('http://185.172.103.40:1337/api/accueil?populate=*');
     console.dir(res.data, { depth: null });
     return res.data;
   } catch (error) {
